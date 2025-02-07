2db22d22a8b769c1dffee3987ad9756bd9abb7a8
well <the.w.3107@gmail.com>
2025-02-07 09:21:12 -0300

    feat: react navigation themes and animation

--- app/modal.tsx
+++ app/modal.tsx
@@ -1,35 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
+import { Pressable, StyleSheet, Text } from 'react-native';
+import Animated, { FadeIn, FadeInDown } from 'react-native-reanimated';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
-
-export default function ModalScreen() {
+export default function Modal() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Modal</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/modal.tsx" />
-
-      {/* Use a light status bar on iOS to account for the black space above the modal */}
-      <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
-    </View>
+    <Animated.View
+      entering={FadeIn}
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#000000d0',
+      }}
+    >
+      {/* Dismiss modal when pressing outside */}
+      <Link href={'../'} asChild>
+        <Pressable style={StyleSheet.absoluteFill} />
+      </Link>
+      <Animated.View
+        entering={FadeInDown}
+        style={{
+          width: '90%',
+          height: '80%',
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: 'white',
+        }}
+      >
+        <Text style={{ fontWeight: 'bold', marginBottom: 10 }}>Modal Screen</Text>
+        <Link href="../">
+          <Text>← Go back</Text>
+        </Link>
+      </Animated.View>
+    </Animated.View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
