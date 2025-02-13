80e254441b2346daeac3d83225e67cec21ae3f11
well <the.w.3107@gmail.com>
2025-02-13 13:32:53 -0300

    feat: fetch battles data

--- src/app/(private)/index.tsx
+++ src/app/(private)/index.tsx
@@ -1,11 +1,40 @@
-import { StyleSheet, View } from "react-native";
+import { supabase } from "@/src/services/supabase";
+import { useEffect, useState } from "react";
+import { FlatList, StyleSheet, View } from "react-native";
 import { Text } from "tamagui";
 
 export default function TabOneScreen() {
+  const [battles, setBattles] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetch = async () => {
+      const { data, error } = await supabase
+        .from("battles")
+        .select("id,winner,user_a(id,nick),user_b(id,nick)");
+
+      console.log({ data, error });
+      return data || [];
+    };
+
+    fetch().then((battles) => setBattles(battles));
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Minhas Batalhas: ({battles.length})</Text>
       <View style={styles.separator} />
+      <FlatList
+        data={battles}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => {
+          return (
+            <View style={{ borderWidth: 1, borderColor: '#fff', display: 'flex', flexDirection: 'row', gap: 8 }}>
+              <Text>{item.user_a.nick}</Text>
+              <Text>{item.user_b.nick}</Text>
+            </View>
+          );
+        }}
+      />
     </View>
   );
 }
