cb41735f217302cd4f41b66605015205b3e5fd8f
well <the.w.3107@gmail.com>
2025-02-19 18:16:36 -0300

    fix: login via discord pelo mobile

--- src/app/(public)/sign-in.tsx
+++ src/app/(public)/sign-in.tsx
@@ -1,9 +1,33 @@
 import { supabase } from "@/src/services/supabase";
 import { Button, Input, Text, View } from "tamagui";
-import { StyleSheet } from "react-native";
+import { Platform, StyleSheet } from "react-native";
 import { useState } from "react";
 import { Link, router } from "expo-router";
 import { useToast } from "react-native-toast-notifications";
+import * as WebBrowser from "expo-web-browser";
+import { makeRedirectUri } from "expo-auth-session";
+import * as QueryParams from "expo-auth-session/build/QueryParams";
+import * as Linking from "expo-linking";
+
+WebBrowser.maybeCompleteAuthSession(); // required for web only
+const redirectTo = makeRedirectUri();
+
+console.log({ redirectTo });
+const createSessionFromUrl = async (url: string) => {
+  const { params, errorCode } = QueryParams.getQueryParams(url);
+
+  if (errorCode) throw new Error(errorCode);
+  const { access_token, refresh_token } = params;
+
+  if (!access_token) return;
+
+  const { data, error } = await supabase.auth.setSession({
+    access_token,
+    refresh_token,
+  });
+  if (error) throw error;
+  return data.session;
+};
 
 export default function SignInPage() {
   const [email, setEmail] = useState("");
@@ -11,21 +35,46 @@ export default function SignInPage() {
   const [loading, setLoading] = useState(false);
   const toast = useToast();
 
+  const url = Linking.useURL();
+  if (url) createSessionFromUrl(url);
+
+  function withLoad<T>(cb: () => Promise<T>) {
+    return () => {
+      setLoading(true);
+      cb().finally(() => setLoading(false));
+    };
+  }
+
   async function signInWithEmail() {
-    setLoading(true);
     const { error } = await supabase.auth.signInWithPassword({
       email: email,
       password: password,
     });
 
     if (error) toast.show(error.message);
-    setLoading(false);
   }
 
   async function signInWithDiscord() {
-    setLoading(true);
-    await supabase.auth.signInWithOAuth({ provider: "discord" });
-    setLoading(false);
+    if (Platform.OS !== "web") {
+      const { data, error } = await supabase.auth.signInWithOAuth({
+        provider: "discord",
+        options: {
+          redirectTo,
+          skipBrowserRedirect: true,
+        },
+      });
+      // if (error) throw error;
+
+      const res = await WebBrowser.openAuthSessionAsync(
+        data?.url ?? "",
+        redirectTo
+      );
+
+      if (res.type === "success") {
+        const { url } = res;
+        await createSessionFromUrl(url);
+      }
+    } else await supabase.auth.signInWithOAuth({ provider: "discord" });
   }
 
   return (
@@ -54,12 +103,12 @@ export default function SignInPage() {
           </Link>
         </View>
         <View style={[styles.verticallySpaced]}>
-          <Button disabled={loading} onPress={() => signInWithEmail()}>
+          <Button disabled={loading} onPress={withLoad(signInWithEmail)}>
             <Text>Logar</Text>
           </Button>
         </View>
         <View style={[styles.verticallySpaced]}>
-          <Button disabled={loading} onPress={() => signInWithDiscord()}>
+          <Button disabled={loading} onPress={withLoad(signInWithDiscord)}>
             <Text>Logar com discord</Text>
           </Button>
         </View>
